808b96b58d9fcd9b102885260baf7b6f068dbd25
Iamneo <techadmin@iamneo.ai>
2023-09-03 05:14:11 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -62,3 +62,18 @@ btns.forEach((btn, i) => {
 });
 
 document.querySelector('#reset').addEventListener('click', resetGame);
+
+function myfunc(){
+
+    var b1,b2,b3,b4,b5,b6,b7,b8,b9;
+    b1 = document.getElementById("b1").value;
+    b2 = document.getElementById("b2").value;
+    b3 = document.getElementById("b3").value;
+    b4 = document.getElementById("b4").value;
+    b5 = document.getElementById("b5").value;
+    b6 = document.getElementById("b6").value;
+    b7 = document.getElementById("b7").value;
+    b8 = document.getElementById("b8").value;
+    b9 = document.getElementById("b9").value;
+
+}
